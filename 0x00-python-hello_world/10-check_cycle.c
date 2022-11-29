7e3b527232e6ad5b1c6ca1c7cab1647239e64d19
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-11-29 15:23:34 +0000

    Correct errors in functions

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,28 @@
-#include 
+#include "lists.h"
+
+/**
+ * check_cycle - varifies the exitence of cycle in list
+ * @ptr: head of list
+ *
+ * Return: 1 cycle exits (success), 0 no cycle (fail)
+ */
+int check_cycle(char *ptr)
+{
+	listint_t *head = ptr;
+	listint_t *traverse = ptr;
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+
+	while ((traverse->next) != NULL)
+	{
+		if (head == (traverse->next))
+		{
+			return (1);
+		}
+		traverse = traverse->next;
+	}
+	return (0);
+}
