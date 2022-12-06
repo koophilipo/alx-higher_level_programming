0353f2bd2901cbc5ee35bfed7085b7a93796fb4e
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-06 23:15:37 +0000

    Correct error in function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@ def element_at(key, val):
     y = len(key)
     if val >= y or val < 0:
         return (None)
-    elif val < y:
+    else:
         for i, num in enumerate(key):
-            if i == val + 1:
-                return (key.index(num))
+            if i == val:
+                return (num)
