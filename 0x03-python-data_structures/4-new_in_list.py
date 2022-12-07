e281443ed9c7617180814183379d11a79fd58b19
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 14:23:28 +0000

    Commmit new_list function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
-    if idx >= 0:
-        y = len(my_list)
-        if idx < y:
-
-    return (new_list)
+    if my_list:
+        new_list = my_list.copy()
+        if idx >= 0:
+            y = len(my_list)
+            if idx < y:
+                new_list.pop(idx)
+                new_list.insert(idx, element)
+        return (new_list)
+    return (my_list)
