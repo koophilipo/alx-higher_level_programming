4cd49cd01ca24a602a81ff2c75614bad986726b8
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 17:17:52 +0000

    Commit multiple_returns functions

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def multiple_returns(sentence):
     ln = 0
     fchar = ""
-    if sentence[0] != None and len(sentence) > 0:
+    if sentence[0] is not None and len(sentence) > 0:
         ln = len(sentence)
         fchar += sentence[0]
     return (ln, fchar)
