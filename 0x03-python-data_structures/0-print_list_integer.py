46763a14c6a2374cf1c275f874f71d2f0fd0495d
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 13:02:30 +0000

    commit print_integer_list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for i, val in enumerate(my_list):
-        print("{}".format(val))
+    if my_list:
+        for i in range(len(my_list)):
+            print("{}".format(my_list[i]))
