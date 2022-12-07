f9e2114bf9a71f31881885e41a9c9f465195e136
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 18:41:13 +0000

    Commit divisible_by_2 function

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#1/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    if my_list:
+        if len(my_list) > 0:
+            new_list = []
+            for i in range(len(my_list)):
+                new_list.append(True if (my_list[i] % 2) == 0 else False)
+            return (new_list)
+    return (None)
