51b51e66cede73ab3e30e3f10c75c5816f9773d8
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 23:21:00 +0000

    Only_diff_elements function

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,7 +3,7 @@
 
 def only_diff_elements(set_1, set_2):
     new_set = set()
-    if set_1 and set_2:
+    if set_1 or set_2:
         if len(set_1) > 0 or len(set_2) > 0:
             diff_set = (set_1 - set_2) | (set_2 - set_1)
             return (diff_set)
