2532cff026999e582d690cc8f57faa147bf3aa9f
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 19:32:11 +0000

    Commit switch function and add test programs

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = 0
+    b = 0
+    if tuple_a and tuple_b:
+        if len(tuple_a) > 0 or len(tuple_b) > 0:
+            for i in range(1):
+    new_tuple = (a, b)
