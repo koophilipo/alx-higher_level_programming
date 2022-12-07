dac787f276306549124d75da8f981d4d6f598867
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 22:53:04 +0000

    Commit common_elements function

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    if set_1 and set_2:
+        if len(set_1) > 0 and len(set_2) > 0:
+            new_set = set_1.intersection(set_2)
+            return (new_set)
+    return (None)
