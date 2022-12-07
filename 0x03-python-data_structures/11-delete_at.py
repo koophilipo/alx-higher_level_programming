ca3f907a2025c7adb0d703ccbcfa7c59756fafb3
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 18:52:40 +0000

    Correct error in delete_at function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,5 +4,10 @@
 def delete_at(my_list=[], idx=0):
     if my_list or idx < 0:
         if len(my_list) > 0 and idx < len(my_list):
-            my_list.pop(idx)
+            new_list = []
+            for i in range(len(my_list)):
+                if i == idx:
+                    pass
+                else:
+                    new_list.append(my_list[i])
     return (my_list)
