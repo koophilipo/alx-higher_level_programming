059254117f2a162851b8e9b6d06080bba0beaa0e
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-07 15:41:39 +0000

    Commit print_matrix function

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        if len(matrix) > 0:
+            for i in range(len(matrix)):
+                for b in range(len(matrix[i])):
+                    if b == 0:
+                        pass
+                    else:
+                        print("", end=' ')
+                    print("{}".format(matrix[i][b]), end='')
+                print("\n", end='')
