86a249035967d1cc371334a8866fe33cefbb65b8
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-19 18:33:50 +0000

    Commit list_division function along with test program

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+
+    new_list = []
+    for i in range(list_length):
+        x = 0
+        try:
+            x = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            new_list.append(x)
+        except ZeroDivisionError:
+            print("division by zero")
+            new_list.append(x)
+        except IndexError:
+            print("out of range")
+            new_list.append(x)
+        else:
+            new_list.append(x)
+    return (new_list)
