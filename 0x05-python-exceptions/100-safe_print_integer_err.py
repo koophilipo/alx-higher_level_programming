a4a52b0ba6108db3623aa92eed895d3c39078179
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-19 19:34:33 +0000

    Commit safe_print_integer function along with test program

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except TypeError as excpt:
+        sys.stderr.write(f"Exception: {excpt}\n")
+        return (False)
+    except ValueError as ept:
+        sys.stderr.write(f"Exception: {ept}\n")
+        return (False)
+    else:
+        return (True)
